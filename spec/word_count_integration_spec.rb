3207e81049a27495705428844bba74fb3066be40
Alyssa Horrocks <alyssa.horrocks2@gmail.com>
2015-08-07 10:27:37 -0700

    Pass all specs and run word_checker successfully.

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -4,11 +4,11 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the word_count path', {:type => :feature}) do
-  it('Counts the number of times a given word appears in a sentence')
+  it('Counts the number of times a given word appears in a sentence') do
     visit('/')
     fill_in('word_count', :with => 'All dogs go to heaven')
     fill_in('word', :with => 'dogs')
-    click_button('Send')
+    click_button('Lets Count')
     expect(page).to have_content(1)
   end
 end
